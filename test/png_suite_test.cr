b01e533e8d9e87d66545677a4c616eb123e3115d
Leon Rische <leon.rische@student.uni-siegen.de>
2016-07-08 22:59:40 +0200

    Refactor test suite

--- test/png_suite_test.cr
+++ test/png_suite_test.cr
@@ -4,9 +4,8 @@ require "../src/stumpy_png"
 
 module StumpyPNG
   class PNGSuiteTest < Minitest::Test
-    def test_basic_formats
-      skip
-      images = Dir["./test/png_suite/basic_formats/*.png"]
+    def image_test_helper(path)
+      images = Dir[path]
       images.each do |image|
         png = PNG.read(image)
         canvas = png.to_canvas
@@ -20,81 +19,26 @@ module StumpyPNG
 
         assert_equal reference, rgba
       end
+    end
+
+    def test_basic_formats
+      image_test_helper("./test/png_suite/basic_formats/*.png")
     end
 
     def test_image_filtering
-      skip
-      images = Dir["./test/png_suite/image_filtering/*.png"]
-
-      images.each do |image|
-        png = PNG.read(image)
-        canvas = png.to_canvas
-
-        reference_path = image.gsub(".png", ".rgba")
-        # FIXME: For some reason -gamma 1 does not work as expected
-        # system "convert -depth 8 -compress none -gamma 0.999999 #{image} #{reference_path}"
-
-        reference = File.read(reference_path).bytes
-        rgba = canvas.pixels.map(&.to_rgba8).map(&.to_a).flatten
-
-        assert_equal reference, rgba
-      end
+      image_test_helper("./test/png_suite/image_filtering/*.png")
     end
 
     def test_chunk_ordering
-      skip
-      images = Dir["./test/png_suite/chunk_ordering/*.png"]
-
-      images.each do |image|
-        png = PNG.read(image)
-        canvas = png.to_canvas
-
-        reference_path = image.gsub(".png", ".rgba")
-        # FIXME: For some reason -gamma 1 does not work as expected
-        # system "convert -depth 8 -compress none -gamma 0.999999 #{image} #{reference_path}"
-
-        reference = File.read(reference_path).bytes
-        rgba = canvas.pixels.map(&.to_rgba8).map(&.to_a).flatten
-
-        assert_equal reference, rgba
-      end
+      image_test_helper("./test/png_suite/chunk_ordering/*.png")
     end
 
     def test_interlacing
-      skip
-      images = Dir["./test/png_suite/interlacing/*.png"]
-
-      images.each do |image|
-        png = PNG.read(image)
-        canvas = png.to_canvas
-
-        reference_path = image.gsub(".png", ".rgba")
-        # FIXME: For some reason -gamma 1 does not work as expected
-        # system "convert -depth 8 -compress none -gamma 0.999999 #{image} #{reference_path}"
-
-        reference = File.read(reference_path).bytes
-        rgba = canvas.pixels.map(&.to_rgba8).map(&.to_a).flatten
-
-        assert_equal reference, rgba
-      end
+      image_test_helper("./test/png_suite/interlacing/*.png")
     end
 
     def test_odd_sizes
-      images = Dir["./test/png_suite/odd_sizes/*.png"]
-
-      images.each do |image|
-        png = PNG.read(image)
-        canvas = png.to_canvas
-
-        reference_path = image.gsub(".png", ".rgba")
-        # FIXME: For some reason -gamma 1 does not work as expected
-        # system "convert -depth 8 -compress none -gamma 0.999999 #{image} #{reference_path}"
-
-        reference = File.read(reference_path).bytes
-        rgba = canvas.pixels.map(&.to_rgba8).map(&.to_a).flatten
-
-        assert_equal reference, rgba
-      end
+      image_test_helper("./test/png_suite/odd_sizes/*.png")
     end
   end
 end
