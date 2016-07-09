55e9407b117cf25955a934ddc3df988105a1db55
Leon Rische <leon.rische@student.uni-siegen.de>
2016-07-10 00:42:45 +0200

    Add png write test

---
+++ test/png_write_test.cr
@@ -0,0 +1,19 @@
+require "minitest/autorun"
+
+require "../src/stumpy_png"
+
+module StumpyPNG
+  class PNGSuiteTest < Minitest::Test
+    def test_png_write
+      image = "./test/png_suite/basic_formats/basn0g01.png"
+      tmp_image = "/tmp/test.png"
+
+      original = StumpyPNG.read(image)
+      StumpyPNG.write(original, tmp_image)
+
+      written = StumpyPNG.read(tmp_image)
+
+      assert_equal original, written
+    end
+  end
+end
