e2a6db43d873b2699e66819e6ebf3ea8be0f9b5b
Leon Rische <leon.rische@student.uni-siegen.de>
2016-07-10 01:04:43 +0200

    Remove old write functions

--- src/stumpy_png/canvas.cr
+++ src/stumpy_png/canvas.cr
@@ -2,8 +2,6 @@ require "./rgba"
 
 module StumpyPNG
   class Canvas
-    ALPHA_CHECKERBOARD_SIZE = 8
-
     getter width : Int32
     getter height : Int32
     getter pixels : Array(RGBA)
@@ -31,45 +29,6 @@ module StumpyPNG
       @width == other.width &&
       @height == other.height &&
       @pixels == other.pixels
-    end
-
-    def write_ppm(path)
-      File.open(path, "w") do |file|
-
-        # Write ppm header
-        file.puts "P3"
-        file.puts "#{@width} #{@height}"
-        file.puts "255"
-
-        @height.times do |y|
-          file.puts (0...@width).map { |x| get_pixel(x, y).to_rgb8.to_a }.flatten.join(" ")
-          # @width.times do |x|
-            # color = get_pixel(x, y)
-
-            # Checkerboard colors
-            # c1 = RGBA.new(25700_u16, 25700_u16, 25700_u16, UInt16::MAX - color.a)
-            # c2 = RGBA.new(37008_u16, 37008_u16, 37008_u16, UInt16::MAX - color.a)
-            # checkerboard = ((y / ALPHA_CHECKERBOARD_SIZE) + (x / ALPHA_CHECKERBOARD_SIZE)).even? ? c1 : c2
-
-            # Mix current color and checkerboard color
-            # pixel = color.add(checkerboard)
-            # pixel = color
-            # file.print pixel.to_rgb8.join(" ")
-          # end
-          # file.
-        end
-      end
-    end
-
-    def write_rgba(path)
-      File.open(path, "w") do |file|
-        @pixels.each do |pixel|
-          pixel.to_rgba8.each do |byte|
-            file.print byte.chr
-            # file.write_byte(byte)
-          end
-        end
-      end
     end
   end
 end
