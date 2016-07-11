5f0a50e75b25f418bdddeff2571fd78f31e3e290
Leon Rische <leon.rische@student.uni-siegen.de>
2016-07-11 20:07:29 +0200

    Add rotate and crop_y functions

--- src/stumpy_png/canvas.cr
+++ src/stumpy_png/canvas.cr
@@ -28,6 +28,35 @@ module StumpyPNG
       end
     end
 
+    def rotate
+      rotated = Canvas.new(@height, @width)
+
+      (0...@width).each do |x|
+        (0...@height).each do |y|
+          rotated.set_pixel(y, x, get_pixel(x, y))
+        end
+      end
+
+      rotated
+    end
+
+    def crop_y(from, to)
+      if from >= to || from < 0 || to > @height
+        raise "Invalid y range #{from}-#{to}"
+      end
+
+      cropped = Canvas.new(@width, to - from)
+
+      (0...@width).each do |x|
+        (from...to).each do |y|
+          pixel = get_pixel(x, y)
+          cropped.set_pixel(x, y-from, pixel)
+        end
+      end
+
+      cropped
+    end
+
     def ==(other)
       self.class == other.class &&
       @width == other.width &&
