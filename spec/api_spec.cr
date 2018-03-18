91733f5a07e377da0534ba2d43f3105b45d093a2
obskyr <powpowd@gmail.com>
2018-03-18 02:25:18 +0100

    Add tests for using preexisting IO objects

---
+++ spec/api_spec.cr
@@ -0,0 +1,24 @@
+require "minitest/autorun"
+require "../src/stumpy_png"
+
+module StumpyPNG
+  class StumpyPNGAPITest < Minitest::Test
+    def test_path_read_write
+      canvas = StumpyPNG.read("./spec/png_suite/basic_formats/basn0g01.png")
+      StumpyPNG.write(canvas, "/tmp/test.png")
+    end
+
+    def test_io_read_write
+      in_io = IO::Memory.new
+      File.open("./spec/png_suite/basic_formats/basn0g01.png", "rb") do |file|
+        IO.copy(file, in_io)
+      end
+      in_io.rewind
+      
+      canvas = StumpyPNG.read(in_io)
+
+      out_io = IO::Memory.new
+      StumpyPNG.write(canvas, out_io)
+    end
+  end
+end
