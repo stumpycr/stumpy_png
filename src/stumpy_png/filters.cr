a98869b11fc9abdd81476dd331bca5538f088f44
Leon Rische <leon.rische@me.com>
2019-09-24 14:02:35 +0200

    Use wrapping addition

--- src/stumpy_png/filters.cr
+++ src/stumpy_png/filters.cr
@@ -7,25 +7,25 @@ module StumpyPNG
       when 1 # Sub
         scanline.each_with_index do |byte, index|
           prior = (index - bpp) < 0 ? 0 : decoded[index - bpp]
-          decoded[index] = byte + prior
+          decoded[index] = byte &+ prior
         end
       when 2 # Up
         scanline.each_with_index do |byte, index|
           above = prior_scanline[index]
-          decoded[index] = byte + above
+          decoded[index] = byte &+ above
         end
       when 3 # Average
         scanline.each_with_index do |byte, index|
           prior = (index - bpp) < 0 ? 0 : decoded[index - bpp]
           above = prior_scanline[index]
-          decoded[index] = byte + ((prior.to_f + above.to_f) / 2).floor.to_u8
+          decoded[index] = byte &+ ((prior.to_f + above.to_f) / 2).floor.to_u8
         end
       when 4 # Paeth
         scanline.each_with_index do |byte, index|
           prior = (index - bpp) < 0 ? 0 : decoded[index - bpp]
           above = prior_scanline[index]
           upper_left = (index - bpp) < 0 ? 0 : prior_scanline[index - bpp]
-          decoded[index] = byte + Utils.paeth_predictor(prior, above, upper_left)
+          decoded[index] = byte &+ Utils.paeth_predictor(prior, above, upper_left)
         end
       else
         raise "Unknown filter type #{filter}"
@@ -41,7 +41,7 @@ module StumpyPNG
       when 1 # Sub
         scanline.each_with_index do |byte, index|
           prior = (index - bpp) < 0 ? 0 : decoded[index - bpp]
-          decoded[index] = byte + prior
+          decoded[index] = byte &+ prior
         end
       when 2 # Up
         scanline.each_with_index do |byte, index|
@@ -50,12 +50,12 @@ module StumpyPNG
       when 3 # Average
         scanline.each_with_index do |byte, index|
           prior = (index - bpp) < 0 ? 0 : decoded[index - bpp]
-          decoded[index] = byte + (prior.to_f / 2).floor.to_u8
+          decoded[index] = byte &+ (prior.to_f / 2).floor.to_u8
         end
       when 4 # Paeth
         scanline.each_with_index do |byte, index|
           prior = (index - bpp) < 0 ? 0 : decoded[index - bpp]
-          decoded[index] = byte + Utils.paeth_predictor(prior, 0, 0)
+          decoded[index] = byte &+ Utils.paeth_predictor(prior, 0, 0)
         end
       else
         raise "Unknown filter type #{filter}"
